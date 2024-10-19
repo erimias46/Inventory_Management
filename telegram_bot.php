90c1ee9be03a579e09b67cd50db58aef2ae79213
Erimias Siraye <88778321+erimias46@users.noreply.github.com>
2024-10-19 11:15:46 +0300

    update bot

--- telegram_bot.php
+++ telegram_bot.php
@@ -6,7 +6,7 @@ require 'vendor/autoload.php'; // Ensure you have installed the Telegram SDK
 use Telegram\Bot\Api;
 
 // Create a new instance of the Telegram API
-$telegram = new Api('7048538445:AAFH9g9L2EHfmH8mHK7N8CPt82INxhdzev0'); // Replace with your Bot Token
+$telegram = new Api('7535344872:AAFoyqpzsXU_c-USN5HOuEV6bV9IYQERb0o'); // Replace with your Bot Token
 
 // Database connection
 include('include/db.php'); // Update this
